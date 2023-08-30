013b47a143b2046f9aae8b2049c8e2eb6f00a7ab
Sharif Uddin <sharifruet@gmail.com>
2023-08-30 11:10:51 +0600

    cart update little

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,7 @@
 // src/components/CartItem.js
 import React from 'react';
 import { Button, Col, Row } from 'react-bootstrap';
+import {FaMinus, FaPlus} from 'react-icons/fa';
 
 const CartItem = ({ item, removeFromCart }) => {
   return (
@@ -12,9 +13,9 @@ const CartItem = ({ item, removeFromCart }) => {
         <h4>{item.itemName}</h4>
         <h6>লেখকঃ {item.af1.authorName}</h6>
         <h6>প্রকাশনীঃ {item.af1.publisherName}</h6>
-        <p>দর: {item.salePrice} টাকা</p>
-        <p>পরিমান: {item.quantity}</p>
-        <p>মোট দাম: {item.quantity*item.salePrice} টাকা</p>
+        <p className="m-0">দর: {item.salePrice} টাকা</p>
+        <p className="m-0">পরিমান: <Button onClick={addQuantity} variant="outline-primary"><FaMinus/> </Button>{item.quantity} <Button variant="outline-primary"><FaPlus/> </Button></p>
+        <p className="m-0">মোট দাম: {item.quantity*item.salePrice} টাকা</p>
         <Button onClick={() => removeFromCart(item.componentId)}>Remove</Button>
       </Col>
     </Row>
