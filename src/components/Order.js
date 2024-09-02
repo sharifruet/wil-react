c9e5b4313659573b2d894ffa99c6bdd4645f5fa7
Sharif uddin <sharifruet@gmail.com>
2024-09-02 10:33:07 +0600

    order added

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import OrderItem from './OrderItem';
-import { Button, Card, Col, Container, Row, Toast } from 'react-bootstrap';
+import { Button, Card, Col, Container, Form, ListGroup, Row, Toast } from 'react-bootstrap';
 import GlobalContext from '../GlobalContext'; // Import GlobalContext
 
 const Order = () => {
@@ -20,19 +20,27 @@ const Order = () => {
   return (
     <Container>
       <h1 className="m-2 border-bottom border-info"> Order </h1>
-      {cart.length === 0 ? (
+      {order.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
         <Row>
+          
           <Col md={8}>
-            {order.map(item => (
-              <OrderItem
-                key={item.componentId}
-                item={item}
-                increaseQuantity={() => addToCart(item)} // Increase quantity by adding to cart
-                decreaseQuantity={() => removeFromCart(item.componentId)} // Decrease quantity or remove item
-              />
-            ))}
+            <h3>Payment Options</h3>
+            <ListGroup>
+              <ListGroup.Item>
+                <Form.Check label="bKash" name="paymentOption" type="radio" id="paymentBkash" />
+                <p>Please make payment to merchant account 01XXXXXXXXX</p>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Form.Check label="Nagad"  name="paymentOption" type="radio" id="paymentCod" />
+                <p>Please make payment to merchant account 01XXXXXXXXX</p>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Form.Check label="Cash-on-Delivery (COD)" name="paymentOption" type="radio" id="paymentCod" />
+                <p>Please make payment to merchant account 01XXXXXXXXX</p>
+              </ListGroup.Item>
+            </ListGroup>
           </Col>
           <Col>
 
@@ -48,7 +56,7 @@ const Order = () => {
 
               <Card.Body>
                 <Button className='m-1' variant="warning" onClick={clearCart}>Clear Order</Button>
-                <Button className='m-1' variant="info" onClick={placeOrder}>Place Order</Button>
+                <Button className='m-1' variant="info" onClick={saveOrder}>Place Order</Button>
               </Card.Body>
             </Card>
           </Col>
